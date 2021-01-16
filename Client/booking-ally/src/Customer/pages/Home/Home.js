e2f6a95c3740e1fb3872cb3a0abe7ff23997b40a
Nicky <nicholas.murray@live.com>
2021-01-16 14:07:02 +0000

    Frontend style updates

--- Client/booking-ally/src/Customer/pages/Home/Home.js
+++ Client/booking-ally/src/Customer/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Venues from '../../widgets/Venues/Venues';
 import RestClient from "../../../utilities/rest/RestClient";
 import { connect } from 'react-redux';
 import NavBar from "../../widgets/NavBar/NavBar";
+import styles from "./home.css"
 
 
 class Home extends Component {
